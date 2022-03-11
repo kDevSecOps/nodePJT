79069d98abc3a7179cedce3190d3180b73930664
unknown <kdevsecops@gmail.com>
2022-03-11 23:52:40 +0900

    first commit

---
+++ test02.js
@@ -0,0 +1,17 @@
+let a = 1
+let b = 2
+
+a+b // 3
+a/b // 0.5
+
+let first = 'Bob'
+let last = 'Lee'
+
+first+last // 'BobLee'
+
+first+' '+last // 'Bob Lee'
+
+first+a // Bob1 -> 문자+숫자를 하면, 숫자를 문자로 바꾼 뒤 수행합니다.
+
+console.log(first)
+console.log(last)
